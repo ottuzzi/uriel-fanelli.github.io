b3df349cab3d41266d8975b1ebc290a75aed7100
Uriel Fanelli <uriel.fanelli@gmail.com>
2015-03-01 13:56:46 +0100

    19667

--- js/index.js
+++ js/index.js
@@ -1,98 +1,101 @@
 /**
- * Main JS file for Casper behaviours
+ * Main JS file for HugoScroll behaviours
  */
+var $post = $('.post'),
+	$first = $('.post.first'),
+	$last = $('.post.last'),
+	$fnav = $('.fixed-nav'),
+	$postholder = $('.post-holder'),
+	$postafter = $('.post-after'),
+	$sitehead = $('#site-head');
 
-/* globals jQuery, document */
-(function ($, sr, undefined) {
+/*globals jQuery, document */
+(function ($) {
     "use strict";
+    function srcTo (e) {
+    	$('html, body').animate({
+			scrollTop: e.offset().top
+		}, 1000);
+    }
+    $(document).ready(function(){
 
-    var $document = $(document),
+        $postholder.each(function (e) {
+        	if(e % 2 != 0)
+        		$(this).addClass("oneOff")
+        })
 
-        // debouncing function from John Hann
-        // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
-        debounce = function (func, threshold, execAsap) {
-            var timeout;
+        $postafter.each(function (e) {
+        	var bg = $(this).parent().css('background-color')
+        	$(this).css('border-top-color', bg)
 
-            return function debounced () {
-                var obj = this, args = arguments;
-                function delayed () {
-                    if (!execAsap) {
-                        func.apply(obj, args);
+        	if(e % 2 == 0)
+        		$(this).css('left', '6%')
+
+        })
+
+
+        $('.btn.first').click( function () {
+        	srcTo ($first)
+        })
+        $('.btn.last').click( function () {
+        	srcTo ($last)
+        })
+        $('#header-arrow').click(function () {
+            srcTo ($first)
+        })
+
+        $('.post-title').each(function () {
+        	var t = $(this).text(),
+        	    index = $(this).parents('.post-holder').index();
+        	$fnav.append("<a class='fn-item' item_index='"+index+"'>"+t+"</a>")
+        	$(this).parents('article').attr('id',t.toLowerCase().split(' ').join('-'));
+        	$('.fn-item').click(function () {
+        		var i = $(this).attr('item_index'),
+        			s = $(".post[item_index='"+i+"']")
+
+        		$('html, body').animate({
+					scrollTop: s.offset().top
+				}, 400);
+
+        	})
+        })
+
+        $('.post.last').next('.post-after').hide();
+        if($sitehead.length) {
+            $(window).scroll( function () {
+                var w = $(window).scrollTop(),
+                    g = $sitehead.offset().top,
+                    h = $sitehead.offset().top + $(this).height()-100;
+
+                if(w >= g && w<=h) {
+                    $('.fixed-nav').fadeOut('fast')
+                } else {
+                    if($(window).width()>500)
+                      $('.fixed-nav').fadeIn('fast')
+                }
+
+                $post.each(function () {
+                    var f = $(this).offset().top,
+                        b = $(this).offset().top + $(this).height(),
+                        t = $(this).parent('.post-holder').index(),
+                        i = $(".fn-item[item_index='"+t+"']"),
+                        a = $(this).parent('.post-holder').prev('.post-holder').find('.post-after');
+
+                     $(this).attr('item_index', t);
+
+                    if(w >= f && w<=b) {
+
+                        i.addClass('active');
+                        a.fadeOut('slow')
+                    } else {
+                        i.removeClass('active');
+                        a.fadeIn('slow')
                     }
-                    timeout = null;
-                }
-
-                if (timeout) {
-                    clearTimeout(timeout);
-                } else if (execAsap) {
-                    func.apply(obj, args);
-                }
-
-                timeout = setTimeout(delayed, threshold || 100);
-            };
-        };
-
-    $document.ready(function () {
-
-        var $postContent = $(".post-content");
-        $postContent.fitVids();
-
-        function updateImageWidth() {
-            var $this = $(this),
-                contentWidth = $postContent.outerWidth(), // Width of the content
-                imageWidth = this.naturalWidth; // Original image resolution
-
-            if (imageWidth >= contentWidth) {
-                $this.addClass('full-img');
-            } else {
-                $this.removeClass('full-img');
-            }
+                })
+            });
         }
-
-        var $img = $("img").on('load', updateImageWidth);
-        function casperFullImg() {
-            $img.each(updateImageWidth);
-        }
-
-        casperFullImg();
-        $(window).smartresize(casperFullImg);
-
-        $(".scroll-down").arctic_scroll();
-
+        $('ul li').before('<span class="bult fa fa-asterisk icon-asterisk"></span>')
+        $('blockquote p').prepend('<span class="quo icon-quote-left"></span>')
+                .append('<span class="quo icon-quote-right"></span>')
     });
-
-    // smartresize
-    jQuery.fn[sr] = function(fn) { return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
-
-    // Arctic Scroll by Paul Adam Davis
-    // https://github.com/PaulAdamDavis/Arctic-Scroll
-    $.fn.arctic_scroll = function (options) {
-
-        var defaults = {
-            elem: $(this),
-            speed: 500
-        },
-
-        allOptions = $.extend(defaults, options);
-
-        allOptions.elem.click(function (event) {
-            event.preventDefault();
-            var $this = $(this),
-                $htmlBody = $('html, body'),
-                offset = ($this.attr('data-offset')) ? $this.attr('data-offset') : false,
-                position = ($this.attr('data-position')) ? $this.attr('data-position') : false,
-                toMove;
-
-            if (offset) {
-                toMove = parseInt(offset);
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top + toMove) }, allOptions.speed);
-            } else if (position) {
-                toMove = parseInt(position);
-                $htmlBody.stop(true, false).animate({scrollTop: toMove }, allOptions.speed);
-            } else {
-                $htmlBody.stop(true, false).animate({scrollTop: ($(this.hash).offset().top) }, allOptions.speed);
-            }
-        });
-
-    };
-})(jQuery, 'smartresize');
+}(jQuery));
